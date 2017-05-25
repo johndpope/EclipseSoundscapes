06009824142488f60e733a93006971e1d8f5372f
Arlindog <arlindo_g@live.com>
2017-05-25 19:05:41 -0400

    Add more Networking functionality

---
+++ EclipseSoundscapes/Networking.swift
@@ -0,0 +1,58 @@
+//
+//  NetworkManager.swift
+//  EclipseSoundscapes
+//
+//  Created by Anonymous on 5/23/17.
+//  Copyright © 2017 DevByArlindo. All rights reserved.
+//
+
+import Foundation
+import Firebase
+import FirebaseStorage
+import FirebaseDatabase
+
+/// Network functionality with Firebase Storeage and RealtimeDB
+/// Uploader
+/// Downloader
+
+
+/// Wrapper for Error specifically for Network Errors
+protocol NetworkErrorProtocol: Error {
+    
+    var localizedTitle: String { get }
+    var localizedDescription: String { get }
+    var code: Int { get }
+}
+
+
+/// Network Error Object
+struct NetworkError: AuthErrorProtocol {
+    
+    var localizedTitle: String
+    var localizedDescription: String
+    var code: Int
+    
+    init(localizedTitle: String?, localizedDescription: String, code: Int) {
+        self.localizedTitle = localizedTitle ?? "Error"
+        self.localizedDescription = localizedDescription
+        self.code = code
+    }
+}
+
+
+/// Upload/Download Status keys
+@objc enum NetworkStatus : Int {
+    case audioSuccess
+    case jsonSuccess
+    case realtimeSuccess
+    case error
+    case cancelled
+    case paused
+}
+
+
+let DIRECTORY = "citizen_scientists"
+
+
+
+
